47dee781c777335bd6ddaa6d5b6404321b791ad2
Prashant Nigam <PrashantNigam@users.noreply.github.com>
2024-09-07 07:45:29 +0000

    [1367] Remove invalid case. Merge conditions.

--- LeetCode/Trees/DFS/LinkedListInBinaryTree.java
+++ LeetCode/Trees/DFS/LinkedListInBinaryTree.java
@@ -19,21 +19,17 @@ public class LinkedListInBinaryTree {
     }
 
     public boolean isSubPath(ListNode head, TreeNode root) {
-        if (head == null)
-            return true;
         if (root == null)
             return false;
-        return dfs(head, root) 
-            || isSubPath(head, root.left) 
+        return dfs(head, root)
+            || isSubPath(head, root.left)
             || isSubPath(head, root.right);
     }
 
     private boolean dfs(ListNode head, TreeNode root) {
         if (head == null)
             return true;
-        if (root == null)
-            return false;
-        if (root.val != head.val)
+        if (root == null || root.val != head.val)
             return false;
         return dfs(head.next, root.left) 
             || dfs(head.next, root.right);
