dc9912c3916f3da0fd72ab71452add4c84873688
Prashant Nigam <PrashantNigam@users.noreply.github.com>
2024-10-20 14:41:37 -0700

    [1106] Implement using a Stack

---
+++ LeetCode/Stack/ParsingABooleanExpression.java
@@ -0,0 +1,58 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+// Affinity Facebook Google Microsoft
+public class ParsingABooleanExpression {
+    public static void main(String[] args) {
+        var ob = new ParsingABooleanExpression();
+        System.out.println(!ob.parseBoolExpr("&(|(f))"));
+        System.out.println(ob.parseBoolExpr("|(f,f,f,t)"));
+        System.out.println(ob.parseBoolExpr("!(&(f,t))"));
+    }
+
+    private static final char AND = '&';
+    private static final char FALSE = 'f';
+    private static final char NOT = '!';
+    private static final char OR = '|';
+    private static final char TRUE = 't';
+
+    public boolean parseBoolExpr(String expression) {
+        var stack = new ArrayDeque<Character>();
+
+        for (var ch : expression.toCharArray())
+            switch (ch) {
+                case ',':
+                    continue;
+                case ')':
+                    processSubexpression(stack);
+                    break;
+                default:
+                    stack.push(ch);
+            }
+
+        return stack.poll() == TRUE;
+    }
+
+    private void processSubexpression(Deque<Character> stack) {
+        var letters = new HashSet<Character>();
+
+        while (stack.peek() != '(')
+            letters.add(stack.pop());
+
+        stack.pop();
+        stack.push(applyOperation(letters, stack.pop()));
+    }
+
+    private char applyOperation(Set<Character> letters, char operation) {
+        return switch (operation) {
+            case AND -> letters.contains(FALSE) ? FALSE : TRUE;
+            case NOT -> letters.contains(TRUE) ? FALSE : TRUE;
+            case OR -> letters.contains(TRUE) ? TRUE : FALSE;
+            default -> 0;
+        };
+    }
+}
